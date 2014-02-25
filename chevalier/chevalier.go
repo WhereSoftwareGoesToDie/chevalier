b95fbd0a94a8b732ff724d92e8c67cb7a60b5779
Sharif Olorin <sio@tesser.org>
2014-02-25 19:37:03 +1100

    Add event loop for req/rep

--- chevalier/chevalier.go
+++ chevalier/chevalier.go
@@ -4,26 +4,40 @@ import (
 	"code.google.com/p/gcfg"
 	"flag"
 	zmq "github.com/pebbe/zmq4"
-	"log"
+	"github.com/anchor/picolog"
+	"os"
+	"log/syslog"
 )
+
+var Logger *picolog.Logger
+
+func handleRequest(sock *zmq.Socket) error {
+	msg, err := sock.RecvMessageBytes(0)
+	Logger.Debugf("Got a request! %v.", msg)
+	return err
+}
 
 func main() {
 	var cfg Config
+	Logger = picolog.NewLogger(syslog.LOG_DEBUG, "chevalier", os.Stdout)
 	configFile := flag.String("cfg", "/etc/chevalier.gcfg", "Path to configuration file. This file should be in gcfg[0] format. [0] https://code.google.com/p/gcfg/")
 	flag.Parse()
 	err := gcfg.ReadFileInto(&cfg, *configFile)
 	if err != nil {
-		log.Fatalf("Could not read config file at %v: %v", *configFile, err)
+		Logger.Fatalf("Could not read config file at %v: %v", *configFile, err)
 	}
 	sock, err := zmq.NewSocket(zmq.REP)
 	if err != nil {
-		log.Fatalf("Could not initialize listen socket: %v", err)
+		Logger.Fatalf("Could not initialize listen socket: %v", err)
 	}
 	err = sock.Bind(cfg.Chevalier.ListenAddress)
 	if err != nil {
-		log.Fatalf("Could not listen on %v: %v", cfg.Chevalier.ListenAddress, err)
+		Logger.Fatalf("Could not listen on %v: %v", cfg.Chevalier.ListenAddress, err)
 	}
-	for true {
-		_, err = sock.RecvMessageBytes(0)
+	reactor := zmq.NewReactor()
+	reactor.AddSocket(sock, zmq.POLLIN, func(e zmq.State) error { return handleRequest(sock) })
+	err = reactor.Run(-1)
+	if err != nil {
+		Logger.Errorf("%v", err)
 	}
 }

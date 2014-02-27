356bc7598fdc885e60591bedd3b82ecf79d0286e
Sharif Olorin <sio@tesser.org>
2014-02-27 13:04:00 +1100

    Actually add indexer and reader files

---
+++ chevalierd/indexer.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	zmq "github.com/pebbe/zmq4"
+	_ "github.com/anchor/chevalier"
+)
+
+func fullUpdate(endpoint string) error {
+	sock, err := zmq.NewSocket(zmq.REQ)
+	if err != nil {
+		return err
+	}
+	err = sock.Connect(endpoint)
+	if err != nil {
+		return err
+	}
+	_, err = sock.Send("", 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func subscribeUpdate(endpoint string) error {
+	sock, err := zmq.NewSocket(zmq.SUB)
+	if err != nil {
+		return err
+	}
+	sock.SetSubscribe("")
+	sock.Connect(endpoint)
+	return nil
+}
+
+func runIndexer(cfg Config) {
+	go fullUpdate(cfg.Vaultaire.ReadEndpoint)
+}

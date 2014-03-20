200b2b132613d3530e4534228bea6e991dd03a42
Sharif Olorin <sio@tesser.org>
2014-03-21 10:57:58 +1100

    travis-ci metadata

---
+++ bin/install_deps.sh
@@ -0,0 +1,19 @@
+# This is here so we can build with Travis; if you just want to use the
+# library then you would probably be better off linking to a packaged
+# version of libmarquise.
+
+sudo apt-get install -y autoconf libtool automake build-essential libglib2.0-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler
+mkdir -p deps/
+cd deps
+
+wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
+tar -xf zeromq-4.0.4.tar.gz
+cd zeromq-4.0.4
+./configure 
+make
+sudo make install
+echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf
+sudo ldconfig
+cd ..
+
+go get code.google.com/p/goprotobuf/{proto,protoc-gen-go}

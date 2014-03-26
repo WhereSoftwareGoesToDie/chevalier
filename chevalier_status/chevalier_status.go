428d59c865fd61abdd00e8c94853719c54fac10a
Sharif Olorin <sio@tesser.org>
2014-03-26 15:49:01 +1100

    Status request utility

---
+++ chevalier_status/chevalier_status.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"github.com/anchor/chevalier"
+	"github.com/anchor/zmqutil"
+	zmq "github.com/pebbe/zmq4"
+	"log"
+	"os"
+)
+
+func main() {
+	endpoint := flag.String("endpoint", "tcp://127.0.0.1:6284", "Chevalier status endpoint (as a ZMQ URI).")
+	flag.Parse()
+	sock, err := zmq.NewSocket(zmq.REQ)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sock.Connect(*endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = zmqutil.RetrySend(sock, "", 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p, err := zmqutil.RetryRecvBytes(sock, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	status, err := chevalier.UnmarshalStatusResponse(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	out, err := status.ToJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(out)
+}

d96f3eeda0744160e163de2196466c10f0bf7f82
Sharif Olorin <sio@tesser.org>
2014-06-10 13:39:17 +1000

    Parse contents entry packet

--- contents.go
+++ contents.go
@@ -2,6 +2,10 @@ package chevalier
 
 import (
 	"errors"
+	"encoding/binary"
+	"bytes"
+	"strings"
+	"fmt"
 )
 
 type RequestOpCode byte
@@ -26,12 +30,54 @@ const (
 
 type ContentsEntry struct {
 	address uint64
-	sources map[string]string
+	tags map[string]string
 }
 
 type ContentsResponse struct {
 	opCode byte
-	entries []ContentsEntry
+	entry *ContentsEntry
+}
+
+func unpackTags(tagData []byte) (map[string]string, error) {
+	tags := make(map[string]string, 0)
+	if len(tagData) == 0 {
+		return tags, nil
+	}
+	sTags := string(tagData[:])
+	pairs := strings.Split(sTags, ",")
+	for _, p := range pairs {
+		idx := strings.Index(p, ":")
+		if idx == -1 || idx+1 == len(p) {
+			return tags, errors.New(fmt.Sprintf("Could not parse tag %v.", p))
+		}
+		field := p[:idx]
+		val := p[idx+1:]
+		tags[field] = val
+	}
+	return tags, nil
+}
+
+func unpackContentsEntry(packet []byte) (*ContentsResponse,error) {
+	buf := bytes.NewBuffer(packet)
+	var addr, contentsLen uint64
+	err := binary.Read(buf, binary.LittleEndian, &addr)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Read(buf, binary.LittleEndian, &contentsLen)
+	if err != nil {
+		return nil, err
+	}
+	e := new(ContentsEntry)
+	e.address = addr
+	e.tags, err = unpackTags(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	r := new(ContentsResponse)
+	r.opCode = byte(ContentsListEntry)
+	r.entry  = e
+	return r, err
 }
 
 func unpackContentsResponse(packet []byte) (*ContentsResponse,error) {
@@ -45,7 +91,7 @@ func unpackContentsResponse(packet []byte) (*ContentsResponse,error) {
 	case InvalidContentsOrigin:
 		return nil, nil
 	case ContentsListEntry:
-		return nil, nil
+		return unpackContentsEntry(packet[1:])
 	case EndOfContentsList:
 		return nil, nil
 	case UpdateSuccess:

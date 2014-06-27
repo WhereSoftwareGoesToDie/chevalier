55b06be542b7d558119cf754aafbe601fdd1e97d
Sharif Olorin <sio@tesser.org>
2014-06-27 10:13:53 +1000

    Add test for contents entry decoding
    Signed-off-by: Sharif Olorin <sio@tesser.org>

--- contents_test.go
+++ contents_test.go
@@ -2,6 +2,8 @@ package chevalier
 
 import (
 	"testing"
+	"bytes"
+	"encoding/binary"
 )
 
 func TestDecodeContentsEnd(t *testing.T) {
@@ -12,5 +14,33 @@ func TestDecodeContentsEnd(t *testing.T) {
 		t.Errorf("Error decoding packet: %v", err)
 	} else if res.opCode != EndOfContentsList {
 		t.Errorf("Wrong opcode: expected EndOfContentsList, got %v", res.opCode)
+	}
+}
+
+func TestDecodeContentsEntry(t *testing.T) {
+	buf := new(bytes.Buffer)
+	buf.WriteByte(2) // ContentsListEntry
+	addr := 42
+	dict := []byte("foo:bar,baz:quux,")
+	dictLen := len(dict)
+	binary.Write(buf, binary.LittleEndian, addr)
+	binary.Write(buf, binary.LittleEndian, dictLen)
+	buf.Write(dict)
+	res, err := unpackContentsResponse(buf.Bytes())
+	if err != nil {
+		t.Errorf("Error decoding packet: %v", err)
+	} else if res.opCode != ContentsListEntry {
+		t.Errorf("Wrong opcode: expected ContentsListEntry, got %v", res.opCode)
+	}
+	if res.entry == nil {
+		t.Errorf("Error parsing contents entry, got a nil pointer")
+	}
+	t.Logf("tags: %v", res.entry.tags)
+	if v, ok := res.entry.tags["foo"]; ok {
+		if v != "bar" {
+			t.Errorf("Corrupted tag: expected bar, got %v (in source dict %v)", v, res.entry.tags)
+		}
+	} else {
+		t.Errorf("Expected tag foo in source dict %v", res.entry.tags)
 	}
 }
